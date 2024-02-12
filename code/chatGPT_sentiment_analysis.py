e1c338853ac4d91ab5b9ae4398b6963755f0e846
Oscar Anderson <117745490+oscar-anderson@users.noreply.github.com>
2024-02-12 17:48:21 +0000

    Update chatGPT_sentiment_analysis.py

--- code/chatGPT_sentiment_analysis.py
+++ code/chatGPT_sentiment_analysis.py
@@ -1,7 +1,29 @@
 '''
-Sentiment Analysis of Tweets Related to ChatGPT with Multinomial Naive Bayes.
-'''
+Sentiment Analysis Script
 
+This script performs a sentiment analysis on a dataset of tweets related to
+OpenAI's Large Language Model (LLM), ChatGPT. Using Natural Language Processing
+(NLP), this script includes a series of functions for loading data,
+preprocessing text, visualising the sentiment and themes within the data and
+fitting the machine learning model to allow for the sentiment of further text
+data to be predicted.
+
+Author: Oscar Anderson
+Date: 11/01/2024
+
+Requirements:
+- pandas
+- matplotlib
+- nltk
+- scikit-learn
+- wordcloud
+
+Usage:
+1. Install the required dependencies using: pip install pandas matplotlib nltk scikit-learn wordcloud.
+2. Update the 'file_path' variable with the path to your CSV file containing tweet data.
+3. Run the script to perform sentiment analysis and visualise the results.
+
+'''
 # Import dependencies.
 import re
 import matplotlib.pyplot as plt
@@ -20,71 +42,107 @@ def load_data(file_path: str, index_col: str = None) -> pd.DataFrame:
     '''
     Load and display data from a CSV file into a pandas DataFrame.
 
-    Parameters:
+    Input:
     - file_path (str): Path to the CSV file.
 
-    Returns:
+    Output:
     - pd.DataFrame: Loaded DataFrame.
     '''
-    df = pd.read_csv(file_path)
-    index_col = index_col
+    df = pd.read_csv(file_path, index_col = index_col)
     print(df.head())
     
     return df
 
-def preprocess_data(data: str, stop_words: list, lemmatizer: WordNetLemmatizer) -> str:
+def preprocess_text(text: str, stop_words: set, lemmatiser: WordNetLemmatizer) -> str:
     '''
     Preprocess tweet data by converting to lowercase, removing URLs, reducing
     words to root form, and removing non-alphanumeric/stop words.
 
-    Parameters:
-    - data (str): Input tweet data.
-    - stop_words (list): List of stop words to be removed.
-    - lemmatizer: Lemmatiser object.
+    Input:
+    - text (str): Input tweet data.
+    - stop_words (set): Set of stop words to be removed.
+    - lemmatiser: Lemmatizer object.
 
-    Returns:
+    Output:
     - str: Processed tweet data.
     '''
-    data = data.lower() # Convert all to lowercase.
-    data = re.sub(r'http\S+', '', data) # Remove any URLs.
-    
+    text = text.lower()  # Convert all to lowercase.
+    text = re.sub(r'http\S+', '', text)  # Remove any URLs.
+
     # Reduce individual words to root form and remove non-alphanumeric/stop words.
-    words = word_tokenize(data)
-    words = [lemmatizer.lemmatize(word) for word in words if word.isalnum() and word not in stop_words]
-    
+    words = word_tokenize(text)
+    words = [lemmatiser.lemmatize(word) for word in words if word.isalnum() and word not in stop_words]
+
     return ' '.join(words)
 
-def explore_data(df: pd.DataFrame, text_data_col: str) -> None:
-    text_data = df[text_data_col]
-    colours = ['skyblue', 'lightcoral', 'lightgreen']
-    text_data.value_counts().plot(kind = 'bar', color = colours) # Inspect sentiment frequencies.
-    plt.title('Data Count for Each Sentiment Label')
-    plt.xlabel('Sentiment label')
-    plt.ylabel('Count')
-    plt.grid()
+def plot_sentiment_freq(labels_data: pd.Series) -> None:
+    '''
+    Plot the distribution of sentiment labels in the dataset.
+
+    Input:
+    - labels_data (pd.Series): Series containing sentiment labels.
+    '''
+    colours = ['lightcoral', 'lightgreen', 'skyblue']
+    sentiment_frequency = labels_data.value_counts()
+
+    ax = sentiment_frequency.plot(kind = 'bar', color = colours)
+    plt.title('Sentiment Distribution Among Chat-GPT Related Tweets',
+              fontweight = 'bold',
+              pad = 10)
+    plt.xlabel('Sentiment label', fontweight = 'bold')
+    plt.ylabel('Frequency', fontweight = 'bold')
     plt.xticks(rotation = 45, ha = 'right')
-    plt.show()
+    plt.yticks(range(0, max(sentiment_frequency) + 10000, 10000))
     
+    for label, count in enumerate(sentiment_frequency):
+        ax.annotate(str(count),
+                    (label, count + 0.1),
+                    ha = 'center',
+                    va = 'bottom',
+                    fontsize = 8)
+    
+    plt.grid(axis = 'y')
+    plt.show()
+
+def generate_word_cloud(text_data: pd.Series) -> None:
+    '''
+    Generate and display a word cloud based on the input text data.
+
+    Input:
+    - text_data (pd.Series): Series containing text data.
+    '''
     text_data_string = ' '.join(text_data)
-    wordcloud = WordCloud(width = 1000, 
+    wordcloud = WordCloud(width = 1000,
                           height = 500,
                           background_color = 'white',
-                          collocations = False) # Generate word cloud.
+                          collocations = False,
+                          colormap = 'winter')
     wordcloud.generate(text_data_string)
+
     plt.figure(figsize = (10, 5))
     plt.imshow(wordcloud)
     plt.axis('off')
-    plt.title('Word Cloud of Tweet Data')
+    plt.title('Word Cloud of ChatGPT-Related Tweet Contents',
+              fontweight = 'bold',
+              pad = 7)
     plt.show()
-    
+
 def fit_model(df: pd.DataFrame, text_data_col: str, labels_col: str) -> None:
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(df[text_data_col])
+    '''
+    Fit a Multinomial Naive Bayes model and display accuracy, confusion matrix, and classification report.
+
+    Input:
+    - df (pd.DataFrame): DataFrame containing the dataset.
+    - text_data_col (str): Name of the column containing processed text data.
+    - labels_col (str): Name of the column containing sentiment labels.
+    '''
+    vectoriser = CountVectorizer()
+    X = vectoriser.fit_transform(df[text_data_col])
     x_train, x_test, y_train, y_test = train_test_split(X, df[labels_col], test_size = 0.2)
     model = MultinomialNB()
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
-    
+
     print('Accuracy:', accuracy_score(y_test, y_pred))
     print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
     print("\nClassification Report:\n", classification_report(y_test, y_pred))
@@ -94,12 +152,14 @@ file_path = 'C:/Users/Oscar/Documents/Projects/chatGPT-sentiment-analysis/chatGP
 df = load_data(file_path, 'index')
 
 # Pre-processing.
-stop_words = stopwords.words('english') # Remove stop words.
-lemmatizer = WordNetLemmatizer()
-df['processed_tweets'] = df['tweets'].apply(lambda tweet: preprocess_data(tweet, stop_words, lemmatizer))
+stop_words = set(stopwords.words('english'))
+lemmatiser = WordNetLemmatizer()
+tweet_data = df['tweets']
+df['processed_tweets'] = tweet_data.apply(lambda tweet: preprocess_text(tweet, stop_words, lemmatiser))
 
 # Exploratory data analysis (EDA).
-explore_data(df, 'processed_tweets')
+plot_sentiment_freq(df['labels'])
+generate_word_cloud(df['processed_tweets'])
 
 # Fit Multinomial Naive Bayes model.
 fit_model(df, 'processed_tweets', 'labels')
